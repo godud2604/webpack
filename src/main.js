be4204b373ccfc8f362a7cea10df68d052b39b6e
Junmo Yoo <jmyoow@gmail.com>
2021-04-09 14:21:20 +0900

    webpack-js-only

---
+++ src/main.js
@@ -0,0 +1,10 @@
+class TextSign {
+	constructor() {
+		this.elem = document.createElement('div');
+		this.elem.innerHTML = 'TextSign!';
+		this.elem.style.transform = `translate(${Math.random() * window.innerWidth * 0.8}px, ${Math.random() * window.innerHeight * 0.8}px)`;
+		document.body.appendChild(this.elem);
+	}
+}
+
+new TextSign();
