d219ec8d5860107510d35fd140a846d6e8ad3df2
Junmo Yoo <jmyoow@gmail.com>
2021-07-27 18:30:15 +0900

    js 분리

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-class TextSign {
-	constructor() {
-		this.elem = document.createElement('div');
-		this.elem.innerHTML = 'TextSign!';
-		this.elem.style.transform = `translate(${Math.random() * window.innerWidth * 0.8}px, ${Math.random() * window.innerHeight * 0.8}px)`;
-		document.body.appendChild(this.elem);
-	}
-}
+import { TextSign } from "./TextSign.js";
 
 new TextSign();
